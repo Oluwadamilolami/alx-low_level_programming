2cc36ac9421f79cddb4f772974b726386b9bc85e
Oluwadamilolami <ojooluwadamilolamary12@gmail.com>
2022-04-21 16:58:21 -0700

    My listh file

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,7 +1,6 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
+#ifndef _LISTS_H_
+#define _LISTS_H_
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -14,7 +13,6 @@
  */
 typedef struct list_s
 {
-
 	char *str;
 	unsigned int len;
 	struct list_s *next;
@@ -22,7 +20,8 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+size_t list_len_recursion(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-#endif
+#endif /* _LISTS_H_ */
